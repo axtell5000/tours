88a7e72a4c8ebb4defd6add6aef955fda42d1598
Stephen Axtell <axtell.stephen@gmail.com>
2020-11-02 12:25:24 +0200

    toggle more or less info, removing a tour item and displaying a call to action if no tours on screen

--- src/App.js
+++ src/App.js
@@ -8,6 +8,11 @@ function App() {
   const [loading, setLoading] = useState(true);
   const [tours, setTours] = useState([]);
 
+  const removeTour = (id) => {
+    const newTours = tours.filter((tour) => tour.id !== id)
+    setTours(newTours)
+  }
+
   const fetchTours = async () => {
     setLoading(true);
 
@@ -25,6 +30,7 @@ function App() {
   // running the fetchTours just once
   useEffect(() => { fetchTours()}, []);
 
+  // loading data
   if (loading) {
     return (
       <main>
@@ -33,10 +39,24 @@ function App() {
     )
   }
 
+  // if no tours left
+  if (tours.length === 0) {
+    return (
+      <main>
+        <div className='title'>
+          <h2>no tours left</h2>
+          <button className='btn' onClick={() => fetchTours()} title="Refresh">
+            refresh
+          </button>
+        </div>
+      </main>
+    )
+  }
+
   // default
   return (
     <main>
-      <Tours tours={tours} />
+      <Tours tours={tours} removeTour={removeTour} />
     </main>
   );
 }
--- src/Tour.js
+++ src/Tour.js
@@ -1,6 +1,8 @@
 import React, { useState } from 'react';
 
-const Tour = ({ id, image, info, name, price }) => {
+const Tour = ({ id, image, info, name, price, removeTour }) => {
+  // setting state
+  const [readMore, setReadMore] = useState(false);
   return (
     <article className="single-tour">
       <img src={image} alt={name} />
@@ -9,8 +11,13 @@ const Tour = ({ id, image, info, name, price }) => {
           <h4>{name}</h4>
           <h4 className="tour-price">${price}</h4>
         </div>
-        <p>{info}</p>
-        <button className="delete-btn" title="not interested">not interested</button>
+        <p>
+          {readMore ? info : `${info.substring(0, 200)}...`}
+          <button onClick={() => setReadMore(!readMore)}>
+            {readMore ? 'show less' : '  read more'}
+          </button>
+        </p>
+        <button className="delete-btn" title="not interested" onClick={() => removeTour(id)}>not interested</button>
       </footer>
     </article>
   );
