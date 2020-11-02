2454d22fa66361295b68b3b495e619e82554baf4
Stephen Axtell <axtell.stephen@gmail.com>
2020-11-02 11:09:25 +0200

    changes to css, setting up state, fetching data from api

--- src/App.js
+++ src/App.js
@@ -4,7 +4,40 @@ import Tours from './Tours';
 const url = 'https://course-api.netlify.app/api/react-tours-project';
 
 function App() {
-  return <h2>Tours Project Setup</h2>;
+  // setting up state with hooks
+  const [loading, setLoading] = useState(true);
+  const [tours, setTours] = useState([]);
+
+  const fetchTours = async () => {
+    setLoading(true);
+
+    try {
+      const response = await fetch(url);
+      const tourData = await response.json();
+      setLoading(false);
+      setTours(tourData);
+    } catch (error) {
+      setLoading(false)
+      console.log(error)
+    }   
+  };
+
+  useEffect(() => { fetchTours()}, []);
+
+  if (loading) {
+    return (
+      <main>
+        <Loading />
+      </main>
+    )
+  }
+
+  // default
+  return (
+    <main>
+      <Tours />
+    </main>
+  );
 }
 
 export default App;
